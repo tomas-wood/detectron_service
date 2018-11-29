06c0c366b857f693f453c54e49907545ba072d39
Thomas Wood <thomas.wood@huawei.com>
2018-11-28 23:53:11 -0800

    almost there or something I guess

--- cpu_server.py
+++ cpu_server.py
@@ -8,7 +8,7 @@ from caffe2.python import workspace, core, dyndep
 
 from detectron.core.config import merge_cfg_from_file, cfg
 import detectron.utils.model_convert_utils as mutils
-import detectron.utils.blobl as blob_utils
+import detectron.utils.blob as blob_utils
 
 
 def crop_center(img,cropx,cropy):
@@ -35,6 +35,23 @@ def rescale(img, input_height, input_width):
         imgScaled = skimage.transform.resize(img, (input_width, input_height))
 
     return imgScaled
+
+
+def _sort_results(boxes, segms, keypoints, classes):
+    indices = np.argsort(boxes[:, -1])[::-1]
+    if boxes is not None:
+        boxes = boxes[indices, :]
+    if segms is not None:
+        segms = [segms[x] for x in indices]
+    if keypoints is not None:
+        keypoints = [keypoints[x] for x in indices]
+    if classes is not None:
+        if isinstance(classes, list):
+            classes = [classes[x] for x in indices]
+        else:
+            classes = classes[indices]
+
+    return boxes, segms, keypoints, classes
 
 
 def _prepare_blobs(
@@ -68,6 +85,16 @@ def _prepare_blobs(
     )
     return blobs
 
+def _get_result_blobs(check_blobs):
+    ret = {}
+    for x in check_blobs:
+        sn = core.ScopedName(x)
+        if workspace.HasBlob(sn):
+            ret[x] = workspace.FetchBlob(sn)
+        else:
+            ret[x] = None
+
+    return ret
 
 ## JUST PLAYING AROUND. KEEPING TRACK OF WHAT NEEDS TO BE DONE.
 
@@ -76,21 +103,105 @@ def load_model():
     the caffe2 workspace. """
 
     device_opts = caffe2_pb2.DeviceOption()
-    device_opts.device_type = caffe2_pb.CPU
+    device_opts.device_type = caffe2_pb2.CPU
     INIT_NET = "out/model_init.pb"
     PREDICT_NET = "out/model.pb"
     init_def = caffe2_pb2.NetDef()
-    with open(INIT_NET) as f:
+    with open(INIT_NET, 'rb') as f:
         init_def.ParseFromString(f.read())
-    init_def.device_option.CopyFrom(device_opts)
-    workspace.RunNetOnce(init_net)
+        init_def.device_option.CopyFrom(device_opts)
+        workspace.RunNetOnce(init_def.SerializeToString())
+
     net_def = caffe2_pb2.NetDef()
-    with open(PREDICT_NET) as f:
+    with open(PREDICT_NET, 'rb') as f:
         net_def.ParseFromString(f.read())
+        net_def.device_option.CopyFrom(device_opts)
+        workspace.CreateNet(net_def.SerializeToString(), overwrite=True)
 
-    net_def.device_option.CopyFrom(device_opts)
-    workspace.CreateNet(net_def.SerializeToString(), overwrite=True)
-
+    return init_def, net_def
 
 def get_image(img_url):
-    img = skimage.img_as_float(skimage.io.imread(img_url)).astype(np.float32)
+    return skimage.img_as_float(skimage.io.imread(img_url)).astype(np.float32)
+
+def run_on_image(img_url, net):
+    img = get_image(img_url)
+    input_blobs = _prepare_blobs(
+                                 img,
+                                 cfg.PIXEL_MEANS,
+                                 cfg.TEST.SCALE,
+                                 cfg.TEST.MAX_SIZE
+                                )
+
+    for k, v in input_blobs.items():
+        workspace.FeedBlob(
+                           core.ScopedName(k),
+                           v,
+                           mutils.get_device_option_cpu()
+                          )
+    workspace.RunNetOnce(net)
+    blob_names = workspace.Blobs()
+    for x in blob_names:
+        print(x)
+    #goods = [x for x in blob_names if x.split('_')[-1] == 'nms']
+    #print(goods)
+    scores = workspace.FetchBlob('score_nms')
+    classids = workspace.FetchBlob('class_nms')
+    boxes = workspace.FetchBlob('bbox_nms')
+    cls_prob = workspace.FetchBlob('cls_prob')
+    bbox_pred = workspace.FetchBlob('bbox_pred')
+ 
+    print(scores)
+    print(classids)
+    print(boxes)
+
+    print("cls_prob: shape {}".format(cls_prob.shape))
+    print(cls_prob)
+    print("bbox_pred: shape {}".format(bbox_pred.shape))
+    print(bbox_pred)
+    #except Exception as e:
+    #    print('Model failed to run')
+    #    R = 0
+    #    scores = np.zeros((R,), dtype=np.float32)
+    #    boxes = np.zeros((R, 4), dtype=np.float32)
+    #    classids = np.zeros((R,), dtype=np.float32)
+
+    boxes = np.column_stack((boxes, scores))
+    #print(boxes)
+    boxes, _, _, classids = _sort_results(
+        boxes, None, None, classids)
+
+    check_blobs = [
+        "result_boxes", "result_classids",  # result
+        ]
+    workspace.FeedBlob('result_boxes', boxes)
+    workspace.FeedBlob('result_classids', classids)
+
+    ret = _get_result_blobs(check_blobs)
+
+    return ret
+
+def load_environment():
+
+    core.GlobalInit(['caffe2', '--caffe2_log_level=0'])
+    DETECTRON_HOME = os.environ.get('DETECTRON_HOME')
+    if DETECTRON_HOME is None:
+        DETECTRON_HOME = '/home/thomas/code/detectron_service/detectron'
+    cfg_fname = os.path.join(
+                DETECTRON_HOME,
+                'configs/12_2017_baselines/e2e_mask_rcnn_R-101-FPN_2x.yaml'
+                          )
+    merge_cfg_from_file(cfg_fname)
+    detectron_ops_lib = "/home/thomas/caffe2/lib/libcaffe2_detectron_ops_gpu.so"
+    dyndep.InitOpsLibrary(detectron_ops_lib)
+    _, net = load_model()
+    return net
+
+def main():
+    #img_url = "https://cdn.japantimes.2xx.jp/wp-content/uploads/2018/03/p9-masangkay-cathat-a-20180319-870x580.jpg"
+    img_url = "https://d3d00swyhr67nd.cloudfront.net/w1200h1200/STF/STF_STKMG_030.jpg"
+    net = load_environment()
+    ret = run_on_image(img_url, net)
+    print(ret)
+
+if __name__ == "__main__":
+    main()
